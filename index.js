bdd31de823ed2077250895da6ed66a84cf01a722
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2020-04-19 22:14:28 -0400

    changing connection to postgres with googles own tutorial

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-
+'use strict'
 require('dotenv').config();
 
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
-
-
-
-
+const process = require('process')
 
 var app = express();
+const Knex = require('knex');
+app.enable('trust proxy');
 
 
 
@@ -40,7 +37,31 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
+// [START cloud_sql_postgres_knex_create]
+// Initialize Knex, a Node.js SQL query builder library with built-in connection pooling.
+const connect = () => {
+  // Configure which instance and what database user to connect with.
+  // Remember - storing secrets in plaintext is potentially unsafe. Consider using
+  // something like https://cloud.google.com/kms/ to help keep secrets secret.
+  const config = {
+    user: process.env.DB_USER, // e.g. 'my-user'
+    password: process.env.DB_PASS, // e.g. 'my-user-password'
+    database: process.env.DB_NAME, // e.g. 'my-database'
+  };
 
+  config.host = `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`;
+
+  // Establish a connection to the database
+  const knex = Knex({
+    client: 'pg',
+    connection: config,
+  });
+
+    return knex;
+};
+
+const knex = connect();
+// [END cloud_sql_postgres_knex_create]
 
 // Heroku post-build script
 // Serve static assets if in production
