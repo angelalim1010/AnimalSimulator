3ef1175162e26ab0b8d1bf4e5d23b1222ccfa75f
Tony Fung <tfung5@gmail.com>
2020-05-06 17:00:23 -0400

    Removed testAction

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Home } from "./components";
-import { getTest } from "./actions/testAction";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  callAPI() {
-    getTest().then((res) => {
-      this.setState({ apiResponse: res.data });
-    });
-  }
-
-  componentDidMount() {
-    this.callAPI();
-  }
   render() {
     return (
       <div className="App">
         <Home />
-        {/* <p className="App-intro">{this.state.apiResponse}</p> */}
       </div>
     );
   }
