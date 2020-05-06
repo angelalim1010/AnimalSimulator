b73076f2d053f91ec70275d2067c147efea68635
Tony Fung <tfung5@gmail.com>
2020-05-06 14:47:49 -0400

    WIP: Working with MaterialUI

--- client/src/components/AnimalCreator.jsx
+++ client/src/components/AnimalCreator.jsx
@@ -1,9 +1,63 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Button, MenuItem, Select, TextField } from "@material-ui/core";
+import AnimalTypes from "../constants/AnimalTypes";
+
+const useStyles = (theme) => ({
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    width: 200,
+  },
+  saveButtonRoot: {
+    backgroundColor: "#90EE90", // lightgreen
+  },
+});
 
 class AnimalCreator extends React.Component {
+  renderAnimalTypeMenuItems = () => {
+    let res = [];
+
+    for (let animalType in AnimalTypes) {
+      res.push(
+        <MenuItem value={animalType} key={animalType}>
+          {AnimalTypes[animalType]}
+        </MenuItem>
+      );
+    }
+
+    return res;
+  };
+
   render = () => {
-    return <div>AnimalCreator</div>;
+    const { classes } = this.props;
+
+    return (
+      <div>
+        <form className={classes.form} noValidate autoComplete="off">
+          <TextField
+            label="Animal Name"
+            variant="outlined"
+            placeholder="Betty" // TODO: Make this the current animal's name
+          />
+          <Select
+            value={
+              Object.keys(AnimalTypes)[0] // TODO: Make this the current animal's type
+            }
+            variant="outlined"
+          >
+            {this.renderAnimalTypeMenuItems()}
+          </Select>
+          <Button
+            variant="contained"
+            classes={{ root: classes.saveButtonRoot }}
+          >
+            Save
+          </Button>
+        </form>
+      </div>
+    );
   };
 }
 
-export default AnimalCreator;
+export default withStyles(useStyles)(AnimalCreator);
