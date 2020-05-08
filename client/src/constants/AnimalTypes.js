64912cb2b392a6c1c3dfdf77d50d8c6fca68e4ef
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2020-05-08 10:47:26 -0400

    adding public urls for cloud storage

--- client/src/constants/AnimalTypes.js
+++ client/src/constants/AnimalTypes.js
@@ -9,33 +9,27 @@
 export default {
   1: {
     name: "Cow",
-    imageURL:
-      "https://storage.cloud.google.com/animalsimulator.appspot.com/animalImages/cow.png?authuser=0&cloudshell=false",
-    soundURL:
-      "https://storage.cloud.google.com/animalsimulator.appspot.com/animalSounds/moo.mp3?authuser=0&cloudshell=false",
+    imageURL: "https://storage.googleapis.com/animalsimulator.appspot.com/animalImages/cow.png",
+    soundURL: "https://storage.googleapis.com/animalsimulator.appspot.com/animalSounds/moo.mp3",
   },
   2: {
     name: "Cat",
-    imageURL:
-      "https://storage.cloud.google.com/animalsimulator.appspot.com/animalImages/cat.png?authuser=0&cloudshell=false",
-    soundURL:
-      "https://storage.cloud.google.com/animalsimulator.appspot.com/animalSounds/meow.mp3?authuser=0&cloudshell=false",
+    imageURL: "https://storage.googleapis.com/animalsimulator.appspot.com/animalImages/cat.png",
+    soundURL: "https://storage.googleapis.com/animalsimulator.appspot.com/animalSounds/meow.mp3",
   },
   3: {
     name: "Dog",
-    imageURL:
-      "https://storage.cloud.google.com/animalsimulator.appspot.com/animalImages/dog.png?authuser=0&cloudshell=false",
-    soundURL:
-      "https://storage.cloud.google.com/animalsimulator.appspot.com/animalSounds/woof.mp3?authuser=0&cloudshell=false",
+    imageURL: "https://storage.googleapis.com/animalsimulator.appspot.com/animalImages/dog.png",
+    soundURL: "https://storage.googleapis.com/animalsimulator.appspot.com/animalSounds/woof.mp3",
   },
   4: {
     name: "Sheep",
-    imageURL: "https://storage.cloud.google.com/animalsimulator.appspot.com/animalImages/sheep.png?authuser=0&cloudshell=false",
-    soundURL: "https://storage.cloud.google.com/animalsimulator.appspot.com/animalSounds/baa.mp3?authuser=0&cloudshell=false",
+    imageURL: "https://storage.googleapis.com/animalsimulator.appspot.com/animalImages/sheep.png",
+    soundURL: "https://storage.googleapis.com/animalsimulator.appspot.com/animalSounds/baa.mp3",
   },
   5: {
     name: "Horse",
-    imageURL: "https://storage.cloud.google.com/animalsimulator.appspot.com/animalImages/horse.png?authuser=0&cloudshell=false",
-    soundURL: "https://storage.cloud.google.com/animalsimulator.appspot.com/animalSounds/neigh.mp3?authuser=0&cloudshell=false",
+    imageURL: "https://storage.googleapis.com/animalsimulator.appspot.com/animalImages/horse.png",
+    soundURL: "https://storage.googleapis.com/animalsimulator.appspot.com/animalSounds/neigh.mp3",
   }
 };
