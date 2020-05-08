367d9a7e0ba5dac844fb90ed42d53687cf4e0173
Tony Fung <tfung5@gmail.com>
2020-05-08 17:48:13 -0400

    Removed blocking client/build line

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, "/client/build")));
-
 const animalsRouter = require("./routes/animals");
 
 app.use("/test", (req, res) => {
