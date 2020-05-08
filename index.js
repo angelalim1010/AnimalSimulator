f9f78faa14f5cb83b967ec22010d204c5a2acbfa
Tony Fung <tfung5@gmail.com>
2020-05-08 19:56:51 -0400

    Revert "Force port to be 8080"
    This reverts commit bcf1cf55479bc2b8f7ae6832d82c1163132eda7c.

--- index.js
+++ index.js
@@ -61,6 +61,8 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-app.listen(8080, () => console.log("App listening on port 8080"));
+app.listen(process.env.PORT || 8080, () =>
+  console.log("App listening on port 8080")
+);
 
 module.exports = app;
