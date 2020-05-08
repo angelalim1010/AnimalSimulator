a092a45ae863845f3b1c5de0d6fd11f1df3fc986
Tony Fung <tfung5@gmail.com>
2020-05-08 17:59:37 -0400

    Changed path.resolve to path.join

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(express.static("client/build"));
 
 // Catch-all GET route
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 // }
 
