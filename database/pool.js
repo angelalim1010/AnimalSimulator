1170889e655a2eaaa1b62381cd7b0797c927024c
Tony Fung <tfung5@gmail.com>
2020-05-08 20:37:48 -0400

    Changed all env variables to DB. Added host check for prod

--- database/pool.js
+++ database/pool.js
@@ -1,9 +1,15 @@
 const Pool = require("pg").Pool;
+
+const host =
+  process.env.NODE_ENV === "production"
+    ? "/cloudsql/" + process.env.INSTANCE_CONNECTION_NAME
+    : process.env.DB_HOST;
+
 const pool = new Pool({
-  user: process.env.SQL_USER,
-  database: process.env.SQL_DATABASE,
-  password: process.env.SQL_PASSWORD,
-  host: "/cloudsql/" + process.env.INSTANCE_CONNECTION_NAME,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  host,
   port: 5432,
 });
 
